820ea6720036afcedf430fc626bdbfe13a94d570
VeronikaSemenkova <nika.siemienkova145@gmail.com>
2023-01-03 15:26:43 +0300

    Project preparation.

---
+++ PomodoroTimer/Sources/App/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  PomodoroTimer
+//
+//  Created by Nika Semenkova on 03.01.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+        let viewController = ViewController()
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+    }
+}
+
